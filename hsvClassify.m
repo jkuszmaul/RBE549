0f6dd16abc8f9f900d7f43fb1baa2d6147c93c94
jsburklund <robomower9508@gmail.com>
2017-12-11 09:19:26 -0500

    Significantly boost speed performance by using matrix ops

--- hsvClassify.m
+++ hsvClassify.m
@@ -1,13 +1,13 @@
 function [classes] = hsvClassify(hsv_img, clusters)
     img_size = size(hsv_img);
-    classes = zeros(img_size(1)*img_size(2),1);
+    class_dists = zeros(img_size(1)*img_size(2), 2);
     % Linearize the image
     pixels = [reshape(hsv_img(:,:,1), img_size(1)*img_size(2),1) ...
              reshape(hsv_img(:,:,2), img_size(1)*img_size(2),1) ...
              reshape(hsv_img(:,:,3), img_size(1)*img_size(2),1)];
-    for i=1:img_size(1)*img_size(2);
-        % Find the closest cluster for each pixel
-        dists = sqrt(sum((clusters - pixels(i,:)).^2,2));
-        [dist, classes(i)] = min(dists);
-    end
+         
+    % Compare the distances and classify
+    class_dists(:,1) = sqrt(sum((pixels - clusters(1,:)).^2,2));
+    class_dists(:,2) = sqrt(sum((pixels - clusters(2,:)).^2,2)); 
+    [dists, classes] = min(class_dists, [], 2);
 end
