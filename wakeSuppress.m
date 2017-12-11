cc914752216587bb481d0e034c8ddb16ca897bd3
jsburklund <robomower9508@gmail.com>
2017-12-11 12:10:30 -0500

    entropy doesn't work as well as expected

--- wakeSuppress.m
+++ wakeSuppress.m
@@ -1,4 +1,4 @@
 function [confidence_image] = wakeSuppress(rgb_image)
   hsv_frame = rgb2hsv(rgb_image);
-  confidence_image = 1-(1-hsv_frame(:,:,2)).*hsv_frame(:,:,3).*((entropyfilt(rgb2gray(rgb_image)/8).^2));
+  confidence_image = 1-(1-hsv_frame(:,:,2)).*hsv_frame(:,:,3);
 end
